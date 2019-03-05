acf88644e025d254ddd8ecb1beac961353998ecf
Maria Fernanda Tutui <tutui@Marias-MacBook-Air.local>
2019-03-05 16:33:54 -0300

    alteracoes lab4

--- Lab04-UART-ADC-Sensor-Reading/Lab04-UART-ADC-Sensor-Reading.ino
+++ Lab04-UART-ADC-Sensor-Reading/Lab04-UART-ADC-Sensor-Reading.ino
@@ -1,27 +1,29 @@
-int val_a, val_d = 0;
-int pin = 3;  
+const int pin = 0;
+float valorSensor = 0;
+float analog_value, digital_value;
+int baud_rate = 9600;
 
 int RMS(int repeat){
-  int accumulated = 0;
-  int digital_value, analog_value, average;
-    
+  float accumulated = 0;
+  float average;
   for (int i = 0; i < repeat; i++){
-    digital_value = analogRead(pin);   //convert the analog-value (voltage) to digital one (0-1024)
-    accumulated = accumulated + digital_value*digital_value;   //accumulate all the digital-values “repeat” times
+    digital_value = analogRead(pin);
+    accumulated =  accumulated + (digital_value * digital_value);
   }
   average = accumulated/repeat;
   return sqrt(average);
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(baud_rate);
 }
 
 void loop() {
-  val_d = RMS(30);    //accumulate x mesures. Recomended x=30;
-  val_a = (val_d*5)/1024;// Regra de 3 para saber o valor analógico
-  
-  Serial.println("Digital:" + val_d);
-  Serial.println("Analógico:" + val_a);
-  delay(2000);
-} 
+  digital_value = RMS(30);
+  analog_value = (digital_value*5)/1024;
+  Serial.print("Digital: ");
+  Serial.println(digital_value);
+  Serial.print("Analógico: ");
+  Serial.println(analog_value);
+  delay(1000);
+}
